80281b672001096a3afc07fc60a9ac003e614888
Riccardo Caraccio <riccardo.caraccio@mail.polimi.it>
2025-02-10 15:06:13 +0100

    refactor: update comments for clarity in main.py

--- main.py
+++ main.py
@@ -6,13 +6,26 @@ from BioSUR.plot import create_triangle_plot
 if __name__ == "__main__":
     ## Example of how to use the BioSUR class without the GUI
     ## Using the create method (recommended)
+
+    # # Create a BioSUR object with the desired composition
     # biosur = BioSUR.create(C=0.50, H=0.050, ASH=0, MOIST=0)
-    # biosur.set_biomass_type(2)# Hardwood
-    # biosur.enable_extrapolation(True)
+
+    # # Set the type of biomass: 0 = OTHERS, 1 = GRASS, 2 = HARDWOOD, 3 = SOFTWOOD
+    # biosur.set_biomass_type(2)
+
+    # # Enable extrapolation if the input composition is outside the triangle of the reference mixtures
+    # biosur.enable_extrapolation(True) 
+
+    # # Calculate the output composition
     # biosur.calculate_output_composition()
+
+    # # Print the output composition
+    # print(biosur.output_composition)
+
+    # # Plot the characterization triangle
     # fig, ax, plot_elements = create_triangle_plot(biosur)
     # plt.show()
-    # print(biosur.output_composition)
-    # Using the GUI
+
+    ## Using the GUI
     app = GUIBioSUR()
     app.mainloop()
